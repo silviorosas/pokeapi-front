e225c345932528cf40ecd86cceae963a37b094c3
silviorosas <silviorosas@yahoo.com.ar>
2024-04-02 20:39:26 -0300

    Modifico baseurl para deploy

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
-  private baseUrl = environment.baseUrl;
+  private baseUrl = 'https://pokeapi-7tdz.onrender.com/api/pokemon';
 
   constructor(private http: HttpClient) { }
 
